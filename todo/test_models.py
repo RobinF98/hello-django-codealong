b0f32afb4b533d33ec2748cb66b265520d0eba5f
RobinF98 <135007436+RobinF98@users.noreply.github.com>
2023-12-08 13:10:01 +0000

    Add extra tests to complete coverage report

--- todo/test_models.py
+++ todo/test_models.py
@@ -7,3 +7,7 @@ class TestModels(TestCase):
     def test_default_done_status_is_false(self):
         item = Item.objects.create(name='Test Todo Item')
         self.assertFalse(item.done)
+
+    def test_item_string_method_returns_name(self):
+        item = Item.objects.create(name='Test Todo Item')
+        self.assertEqual(str(item), item.name)
